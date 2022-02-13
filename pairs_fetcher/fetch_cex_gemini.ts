dfd08351642a19e4a850b94efc7d413afc93b604
Ben Yeoh <shuttlecork@gmail.com>
2022-02-13 17:02:19 +0800

    Updates to gemini

--- pairs_fetcher/fetch_cex_gemini.ts
+++ pairs_fetcher/fetch_cex_gemini.ts
@@ -1,21 +1,24 @@
-import {WebSocket} from "ws";
+#!/usr/bin/env -S npx ts-node
+import { WebSocket } from "ws";
 
 let ws = new WebSocket("wss://api.gemini.com/v2/marketdata");
 
 ws.on("open", () => {
-  console.log("On open");
+    console.log("On open");
     const subscribeMsg = {
         "type": "subscribe",
         "subscriptions": [
-            { "name": "l2", "symbols": [ "ETHUSD" ] }
+            { "name": "l2", "symbols": ["ETHUSD", "BTCUSD", "BTCETH"] }
         ]
     };
     ws.send(JSON.stringify(subscribeMsg));
 });
 
 ws.on("message", (msg) => {
-    ws.close();
+    //ws.close();
     let data = JSON.parse(msg);
-    console.log(`On message: ${data["type"]}```);
+    if (data.trades !== undefined) {
+        console.log(`On message: ${data.type}, ${JSON.stringify(data.trades, null, 4)}`);
+    }
     //ws.close();
 });
